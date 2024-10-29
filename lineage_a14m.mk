d881de226627eecb556671f5cff9faf2587395f8
DeeKatsanos <deekatsanos@gmail.com>
2024-10-29 13:17:54 +0200

    a14m: fix mk typo

--- lineage_a14m.mk
+++ lineage_a14m.mk
@@ -17,7 +17,7 @@ $(call inherit-product, device/samsung/a14m/device.mk)
 PRODUCT_DEVICE := a14m
 PRODUCT_NAME := lineage_a14m
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Samsung Galaxy A14
+PRODUCT_MODEL := Galaxy A14
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
